5e2601c23e9ca05f830128d47007bc0d8aeb60a9
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-03-19 19:36:22 +0300

    arm64-linux and related refactoring (2)

--- cmake/arm64-linux.cmake
+++ cmake/arm64-linux.cmake
@@ -1,5 +1,7 @@
 # CMake Toolchain file for crosscompiling on ARM.
-#
+# Assumed:
+# sudo apt-get install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu binutils-aarch64-linux-gnu
+
 # Target operating system name
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
@@ -11,4 +13,3 @@ set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
 # Where to look for the target environment
 set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
