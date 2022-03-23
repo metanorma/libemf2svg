10b84aa8fc760d28159172fab79b6b838949a4f4
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-03-23 19:28:52 +0300

    Added macos arm64 workflow (7)

---
+++ cmake/arm64-osx.cmake
@@ -0,0 +1,14 @@
+# CMake Toolchain file for crosscompiling on arm64(aks aarch64) macos.
+
+# Target operating system name
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
+
+# Names of compilers
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_OSX_ARCHITECTURES arm64)
+
+# Where to look for the target environment
+#set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
